0b021c2df7f22b21e9b243fe636dfb7d1e9f0679
Jay Ramoliya <ramoliyajay9@gmail.com>
2024-11-10 17:09:25 +0530

    image change

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -1,14 +1,14 @@
 export const data = [
     {
         "id": 1,
-        "imageUrl": "/image/Simple Landing Shoes.JPG",
+        "imageUrl": "Simple-Landing-Shoes.JPG",
         "demoLink": "https://simpleshoelandingpage.netlify.app/",
         "title": "Simple Shoes Landing Page",
         "language": "React Js"
     },
     {
         "id": 2,
-        "imageUrl": "/image/Dice Game.JPG",
+        "imageUrl": "Dice-Game.JPG",
         "demoLink": "https://dice-game-page-react.netlify.app/",
         "title": "Dice Game",
         "language": "React Js"
@@ -23,14 +23,14 @@ export const data = [
     },
     {
         "id": 4,
-        "imageUrl": "/image/Dashboard.JPG",
+        "imageUrl": "Dashboard.JPG",
         "demoLink": "https://dashboard-page-react.netlify.app/",
         "title": "Dashboard Page",
         "language": "React Js"
     },
     {
         "id": 5,
-        "imageUrl": "/image/Foody Zone.JPG",
+        "imageUrl": "Foody-Zone.JPG",
         "demoLink": "https://foody-zone-1-xyf6.onrender.com/",
         "title": "Foody Zone",
         "language": "React Js"
